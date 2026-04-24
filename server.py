8fdf5a88095804b338c22323132d22b8e5561a98
Nade Ali <nadeali19@gmail.com>
2026-04-24 13:19:17 +0500

    Add pharma backend files

---
+++ server.py
@@ -0,0 +1,81 @@
+from flask import Flask, jsonify, request, send_file
+from pymongo import MongoClient
+
+app = Flask(__name__)
+CORS(app)
+
+MONGO_URI = "mongodb+srv://nadeali426:Alinade1926@cluster0.wml3oa4.mongodb.net/?appName=Cluster0"
+client = MongoClient(MONGO_URI)
+db = client['pharma_db']
+
+initial_items = [
+  {"id":"I001","name":"Paracetamol 500mg","salt":"Paracetamol","company":"Cipla","pack":"10T","rate":25,"mrp":30,"gst":5,"stock":500,"batches":[{"b":"B231","exp":"12/26","qty":200,"cost":18},{"b":"B241","exp":"06/27","qty":300,"cost":19}]},
+  {"id":"I002","name":"Amoxicillin 250mg","salt":"Amoxicillin","company":"Ranbaxy","pack":"10C","rate":85,"mrp":100,"gst":12,"stock":200,"batches":[{"b":"C101","exp":"03/26","qty":100,"cost":60}]},
+  {"id":"I003","name":"Metformin 500mg","salt":"Metformin HCl","company":"Sun Pharma","pack":"10T","rate":45,"mrp":55,"gst":5,"stock":350,"batches":[{"b":"M001","exp":"01/27","qty":350,"cost":32}]},
+  {"id":"I004","name":"Atorvastatin 10mg","salt":"Atorvastatin","company":"Lupin","pack":"10T","rate":120,"mrp":145,"gst":12,"stock":150,"batches":[{"b":"A001","exp":"11/26","qty":150,"cost":88}]},
+  {"id":"I005","name":"Pantoprazole 40mg","salt":"Pantoprazole","company":"USV","pack":"10T","rate":95,"mrp":115,"gst":5,"stock":280,"batches":[{"b":"P001","exp":"08/27","qty":280,"cost":70}]},
+  {"id":"I006","name":"Azithromycin 500mg","salt":"Azithromycin","company":"Cipla","pack":"3T","rate":180,"mrp":220,"gst":12,"stock":90,"batches":[{"b":"Z001","exp":"05/26","qty":90,"cost":130}]},
+  {"id":"I007","name":"Cetirizine 10mg","salt":"Cetirizine HCl","company":"GSK","pack":"10T","rate":35,"mrp":42,"gst":5,"stock":400,"batches":[{"b":"CE01","exp":"02/27","qty":400,"cost":25}]},
+  {"id":"I008","name":"Omeprazole 20mg","salt":"Omeprazole","company":"Torrent","pack":"10C","rate":60,"mrp":75,"gst":5,"stock":320,"batches":[{"b":"OM01","exp":"07/27","qty":320,"cost":44}]},
+  {"id":"I009","name":"Metronidazole 400mg","salt":"Metronidazole","company":"Cipla","pack":"10T","rate":28,"mrp":35,"gst":5,"stock":180,"batches":[{"b":"MT01","exp":"04/26","qty":80,"cost":18}]},
+  {"id":"I010","name":"Amlodipine 5mg","salt":"Amlodipine","company":"Zydus","pack":"10T","rate":55,"mrp":68,"gst":5,"stock":260,"batches":[{"b":"AM01","exp":"06/27","qty":260,"cost":40}]},
+]
+
+initial_users = [{"u":"admin","p":"admin123","r":"admin"},{"u":"user1","p":"1234","r":"user"}]
+initial_parties = [{"name":"Medico Suppliers","type":"Supplier","bal":15000},{"name":"City General Hospital","type":"Customer","bal":-2300},{"name":"Health Distributor","type":"Supplier","bal":8500}]
+
+def initialize_db():
+    if db.items.count_documents({}) == 0:
+        db.items.insert_many(initial_items)
+    if db.users.count_documents({}) == 0:
+        db.users.insert_many(initial_users)
+    if db.parties.count_documents({}) == 0:
+        db.parties.insert_many(initial_parties)
+
+def sterilize(docs):
+    res = []
+    for doc in docs:
+        if '_id' in doc:
+            doc['_id'] = str(doc['_id'])
+        res.append(doc)
+    return res
+
+@app.before_request
+def setup():
+    initialize_db()
+
+@app.route('/')
+def serve_index():
+    return send_file('marg_pharma.html')
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    items = sterilize(list(db.items.find({})))
+    bills = sterilize(list(db.bills.find({})))
+    users = sterilize(list(db.users.find({})))
+    parties = sterilize(list(db.parties.find({})))
+    return jsonify({"items": items, "bills": bills, "users": users, "parties": parties})
+
+@app.route('/api/saveBill', methods=['POST'])
+def save_bill():
+    data = request.json
+    bill = data.get('bill')
+    updated_items = data.get('items')
+    
+    if not bill or not updated_items:
+        return jsonify({"error": "Missing bill or items data"}), 400
+
+    # Insert bill
+    db.bills.insert_one(bill)
+    
+    # Update stock for items
+    for item in updated_items:
+        db.items.update_one(
+            {"id": item["id"]},
+            {"$set": {"stock": item["stock"]}}
+        )
+        
+    return jsonify({"success": True})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
